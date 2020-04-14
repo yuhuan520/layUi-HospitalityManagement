fe09f60def6a7b0d6f4fb5a0a8b03e2f1ea14340
yuhuan520 <2601644091@qq.com>
2020-04-14 20:51:11 +0800

    组员测试第一次提交TestB

---
+++ src/main/java/com/java/springBoot/interceptor/TestB.java
@@ -0,0 +1,12 @@
+package com.java.springBoot.interceptor;
+
+/**
+ * description:
+ * author:余焕
+ * time:2020/4/14 20:49
+ */
+public class TestB {
+    public static void main(String[] args) {
+        System.out.println("组员测试testB..");
+    }
+}
